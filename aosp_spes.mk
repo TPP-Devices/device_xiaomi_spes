3db7084068f19c7e21fbf7ebdac757bff5586704
Yograt <107334627+Yograt@users.noreply.github.com>
2025-05-01 19:06:48 +0700

    spes: Removed Leica Camera and fixes a few bring up.
    (cherry picked from commit 3d2881e4bd68ce02364291f246ee228d6b3a67b7)

--- aosp_spes.mk
+++ aosp_spes.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common EvolutionX stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixel Project stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := lineage_spes
+PRODUCT_NAME := aosp_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -26,7 +26,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Boot Animaton
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# EvolutionX Stuff
+# The Pixel Project Stuff
+CUSTOM_BUILD_TYPE := OFFICIAL
 WITH_GMS := true
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="spes_global-user 13 TKQ1.221114.001 V816.0.8.0.TGCMIXM release-keys" \
