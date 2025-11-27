39bd2b6eb778480c7501174551b929bb5c56e08c
Yograt <fedrix56@gmail.com>
2025-11-27 19:21:00 +0000

    spes: aosp_spes: Fixed wrong inherit dir
    Signed-off-by: Yograt <fedrix56@gmail.com>

--- aosp_spes.mk
+++ aosp_spes.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Inherit some common Pixel Project stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/tpp/config/common_full_phone.mk)
 
 # Product Specifics
 PRODUCT_NAME := aosp_spes
