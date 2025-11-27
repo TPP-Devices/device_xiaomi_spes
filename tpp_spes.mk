34aa7823dc935a6cbc0d82ba262800a74f6b034a
Yograt <fedrix56@gmail.com>
2025-11-27 19:40:25 +0000

    spes: changed inherit from aosp to tpp
    Signed-off-by: Yograt <fedrix56@gmail.com>

---
+++ tpp_spes.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from spes/spesn device
+$(call inherit-product, device/xiaomi/spes/device.mk)
+
+# Inherit some common Pixel Project stuff.
+$(call inherit-product, vendor/tpp/config/common_full_phone.mk)
+
+# Product Specifics
+PRODUCT_NAME := aosp_spes
+PRODUCT_DEVICE := spes
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 11
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Boot Animaton
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# The Pixel Project Stuff
+TPP_BUILD_TYPE := OFFICIAL
+WITH_GMS := true
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="spes_global-user 13 TKQ1.221114.001 V816.0.8.0.TGCMIXM release-keys" \
+    BuildFingerprint=Redmi/spes_global/spes:13/TKQ1.221114.001/V816.0.8.0.TGCMIXM:user/release-keys
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
+# Fingerprint
+BUILD_FINGERPRINT := "Redmi/spes/spes:13/TKQ1.221114.001/V816.0.8.0.TGCMIXM:user/release-keys"
